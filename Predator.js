df11093d8e41b01e1909b71b8158578c1acdb14f
Casey Reed <casey.w.reed@gmail.com>
2016-07-27 10:08:26 -0500

    Completed Step 4. Predator.JS complete.

--- Predator.js
+++ Predator.js
@@ -3,4 +3,25 @@ var Predator = (function () {
   var carnivores = []
   var herbivores = []
 
+  return {
+    getCarnivores: function (callback) {
+      var xhr = new XMLHttpRequest()
+      xhr.open("GET", "carnivores.json")
+      xhr.addEventListener("load", function() {
+        carnivores = JSON.parse(xhr.responseText)
+        callback(carnivores) //Execute the callback within this function or else you don't have access to it
+      })
+      xhr.send()
+
+    },
+    getHerbivores: function (callback) {
+      var xhr = new XMLHttpRequest()
+      xhr.open("GET", "herbivores.json")
+      xhr.addEventListener("load", function() {
+        herbivores = JSON.parse(xhr.responseText)
+        callback(herbivores) //Execute the callback within this function or else you don't have access to it
+      })
+      xhr.send()
+    }
+  }
 }())
